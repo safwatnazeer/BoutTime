8f557f92a5876502b94c26cadcc96c28b795a176
Safwat Shenouda <bingoapps@hotmail.com>
2016-09-11 01:33:26 +0200

    Initial Commit

---
+++ BoutTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoutTime
+//
+//  Created by Safwat Shenouda on 11/09/16.
+//  Copyright © 2016 Safwat Shenouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
